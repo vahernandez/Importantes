53e3e739ace14f8cd37abf15472e7f043a512888
admin <valentin.hdz@gmail.com>
2025-05-25 13:10:29 -0600

    remove Meraki folder from GH

--- Meraki/Menu/opcion1.py
+++
@@ -1,6 +0,0 @@
-# opcion1.py
-
-def opcion1_func():
-    print("Ha seleccionado la opción 1")
-    # Aquí colocas la funcionalidad que deseas para la opción 1
-    
--- Meraki/Menu/opcion2.py
+++
@@ -1,5 +0,0 @@
-# opcion2.py
-
-def opcion2_func():
-    print("Ha seleccionado la opción 2")
-    # Aquí colocas la funcionalidad que deseas para la opción 2
